e8b7ed7f0a2cf1ed08e946b9e15670d6e9fc73c0
dev-yoonjung <dear__yoon@naver.com>
2022-03-15 19:45:58 +0900

    chore: Create a spring boot project
    Created a spring-boot project. This project was created for the purpose of providing api for infinite scroll in antd-table

---
+++ src/main/java/antd/table/infinite/scroll/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package antd.table.infinite.scroll.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
